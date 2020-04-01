8ec0765cd111d2ff6db1d6f50ab18683dfe78a51
ederminio <ederminiops1996@gmail.com>
2020-04-01 19:27:25 +0100

    Intellij version

---
+++ SpringMVCv2/src/ederminio/com/config/WebConfiguration.java
@@ -0,0 +1,22 @@
+package ederminio.com.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfiguration extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {DispatcherServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
