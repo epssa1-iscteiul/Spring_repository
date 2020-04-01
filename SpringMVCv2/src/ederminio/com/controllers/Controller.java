80f6cc455d02646af6817dc0cd1f94bf4d4b3eab
ederminio <ederminiops1996@gmail.com>
2020-04-01 19:28:01 +0100

    Intellij version

---
+++ SpringMVCv2/src/ederminio/com/controllers/Controller.java
@@ -0,0 +1,23 @@
+package ederminio.com.controllers;
+
+import ederminio.com.process.ProcessInformation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	@RequestMapping("/add")
+	public ModelAndView getModelAndView(@RequestParam("t1") long n1 ,@RequestParam("t2") long n2, HttpServletRequest req, HttpServletResponse resp) {		
+		ModelAndView mv=new ModelAndView();
+		ProcessInformation p=new ProcessInformation();
+		mv.setViewName("viewDisplay");
+		mv.addObject("resul",p.add(n1, n2));	
+		return mv;
+	}
+
+}
---
+++ SpringMVCv2/src/ederminio/com/process/ProcessInformation.java
@@ -0,0 +1,7 @@
+package ederminio.com.process;
+
+public class ProcessInformation {
+	
+	public long add(long n1, long n2) {return n1+n2;}
+
+}
